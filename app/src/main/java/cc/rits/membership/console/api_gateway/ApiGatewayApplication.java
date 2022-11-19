1c97a775612350c8d5758f7386b0f625591677c2
averak <abe12@mccc.jp>
2022-11-19 19:10:42 +0900

    #5 API Gatewayをセットアップ

--- app/src/main/java/cc/rits/membership/console/api_gateway/ApiGatewayApplication.java
+++ app/src/main/java/cc/rits/membership/console/api_gateway/ApiGatewayApplication.java
@@ -1,26 +1,13 @@
 package cc.rits.membership.console.api_gateway;
 
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationBeanNameGenerator;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(nameGenerator = ApiGatewayApplication.FQCNBeanNameGenerator.class)
 public class ApiGatewayApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
-    }
-
-    static class FQCNBeanNameGenerator extends AnnotationBeanNameGenerator {
-
-        @Override
-        protected String buildDefaultBeanName(final BeanDefinition definition) {
-            return definition.getBeanClassName();
-        }
-
     }
 
 }
